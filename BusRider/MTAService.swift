3f8bc8ecfebdc2dc1645a89336dfb45839c976d2
Rob Seward <robseward@gmail.com>
2017-12-18 16:45:23 -0500

    Init pods. Draft Moya service router

---
+++ BusRider/MTAService.swift
@@ -0,0 +1,52 @@
+//
+//  MTAService.swift
+//  BusRider
+//
+//  Created by Robert Seward on 12/18/17.
+//  Copyright © 2017 Robert Seward. All rights reserved.
+//
+
+import Foundation
+import Moya
+import Alamofire
+
+enum MTAService {
+    case stopsForLocation(lat: Float, lon: Float, latSpan: Float, lonSpan: Float)
+}
+
+extension MTAService: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://bustime.mta.info/api/where/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .stopsForLocation(_, _, _, _): return "stops-for-location.json"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .stopsForLocation(_, _, _, _): return .get
+        }
+    }
+    
+    var sampleData: Data {
+        switch self {
+        case .stopsForLocation(_,_,_,_): return "".data(using: String.Encoding.utf8)!
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .stopsForLocation(let lat, let lon, let latSpan, let lonSpan):
+            return Task.requestParameters(parameters: ["lat" : lat, "lon" : lon, "latSpan" : latSpan, "lonSpan" : lonSpan], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BusRider' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BusRider
+  pod 'Alamofire', '~> 4.5'
+  pod 'Moya'
+
+  target 'BusRiderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'BusRiderUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
