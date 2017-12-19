e0622f53b29fcc65fe55b030d78626e82379c97f
Rob Seward <robseward@gmail.com>
2017-12-19 11:45:09 -0500

    Draft stop and route models

--- BusRider/StopModel.swift
+++ BusRider/StopModel.swift
@@ -7,15 +7,52 @@
 //
 
 import Foundation
+import Moya_SwiftyJSONMapper
+import SwiftyJSON
 
-protocol Model {
-    init(json: Data)
+
+class StopModel : ALSwiftyJSONAble {
+    let code: String
+    let name: String
+    let routes: [RouteModel]
+    
+    required init?(jsonData: JSON) {
+        if
+            let code = jsonData["code"].string,
+            let name = jsonData["name"].string
+        {
+            self.code = code
+            self.name = name
+            self.routes = RouteModel.routeCollection(jsonData: jsonData["routes"])
+        } else {
+            return nil
+        }
+    }
 }
 
-class StopModel : Model {
-
+class RouteModel : ALSwiftyJSONAble {
+    let longName: String
+    let shortName: String
     
-    required init(json: Data) {
-        
+    static func routeCollection(jsonData: JSON) -> [RouteModel] {
+        var routes = [RouteModel]()
+        for (_,subJson):(String, JSON) in jsonData {
+            if let route = RouteModel(jsonData: subJson) {
+                routes.append(route)
+            }
+        }
+        return routes
+    }
+    
+    required init?(jsonData: JSON) {
+        if
+            let longName = jsonData["longName"].string,
+            let shortName = jsonData["shortName"].string
+        {
+            self.longName = longName
+            self.shortName = shortName
+        } else {
+            return nil
+        }
     }
 }
