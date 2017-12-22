c3595c096cae8059860efb50d3a48f0afefcbe48
Rob Seward <robseward@gmail.com>
2017-12-21 22:08:06 -0500

    formatting

--- BusRider/BusInfoProvider.swift
+++ BusRider/BusInfoProvider.swift
@@ -14,16 +14,14 @@ class BusInfoProvider {
     let provider = MoyaProvider<MTAService>()
     
     func nearbyBusLines(lat: Float, lon: Float) -> PrimitiveSequence<SingleTrait, [String]> {
-    
-            let request = MTAService.stopsForLocation(lat: lat, lon: lon, latSpan: 0.005, lonSpan: 0.005)
-            let observable = provider.rx.request(request)
-                .map(to: StopList.self)
-                .map { stopList -> [String] in
-                    var h = Set<String>()
-                    stopList.stops.forEach { h.update(with: $0.routes[0].shortName) }
-                    return Array(h)
-                }
-            return observable
-    
+        let request = MTAService.stopsForLocation(lat: lat, lon: lon, latSpan: 0.005, lonSpan: 0.005)
+        let observable = provider.rx.request(request)
+            .map(to: StopList.self)
+            .map { stopList -> [String] in
+                var h = Set<String>()
+                stopList.stops.forEach { h.update(with: $0.routes[0].shortName) }
+                return Array(h)
+            }
+        return observable
     }
 }
