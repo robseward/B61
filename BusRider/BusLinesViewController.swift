f5eb7011f558d7b081e90e1cc81a0e592505a76f
Rob Seward <robseward@gmail.com>
2017-12-26 12:01:03 -0500

    Got bus lines displaying in tableview

--- BusRider/BusLinesViewController.swift
+++ BusRider/BusLinesViewController.swift
@@ -24,34 +24,19 @@ class BusLinesViewController: UIViewController, UITableViewDelegate {
         let lat: Float = 40.6881291027667
         let lon: Float = -73.96751666498955
         
-        items.asObservable().bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
-            cell.textLabel?.text = "\(element) @ row \(row)"
+        items.asObservable().bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) { (_, element, cell) in
+            cell.textLabel?.text = "\(element)"
             }
             .disposed(by: disposeBag)
 
-//        let provider = MoyaProvider<MTAService>()
-//
-//        let request = MTAService.stopsForLocation(lat: lat, lon: lon, latSpan: 0.005, lonSpan: 0.005)
-//        provider.rx.request(request)
-//            .map(to: StopList.self)
-//            .map { stopList -> [String] in
-//                var h = Set<String>()
-//                stopList.stops.forEach { h.update(with: $0.routes[0].shortName) }
-//                return Array(h)
-//            }
-//            .subscribe(onSuccess: { (strings) in
-//                print(strings)
-//            }) { (error) in
-//                print(error.localizedDescription)
-//            }.disposed(by: bag)
         
-//        let provider = BusInfoProvider()
-//        provider.nearbyBusLines(lat: lat, lon: lon)
-//            .subscribe(onSuccess: { (strings) in
-//                print(strings)
-//            }, onError: {error in
-//                print(error.localizedDescription)
-//            }).disposed(by: disposeBag)
+        let provider = BusInfoProvider()
+        provider.nearbyBusLines(lat: lat, lon: lon)
+            .subscribe(onSuccess: { (strings) in
+                self.items.value = strings
+            }, onError: {error in
+                print(error.localizedDescription)
+            }).disposed(by: disposeBag)
 
         
     }
